79b77e1b93697b17f907b763e33822d62782be39
Taliesin Sisson <taliesins@yahoo.com>
2022-12-08 22:49:56 +0000

    only include if integration tests are running

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,3 +1,6 @@
+//go:build integration
+// +build integration
+
 package provider
 
 import (
@@ -31,12 +34,6 @@ func TestProvider(t *testing.T) {
 	if err := New(version, commit)().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
-}
-
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
 }
 
 func escapeForHcl(value string) string {
