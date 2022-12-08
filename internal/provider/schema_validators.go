5690dc4875cc4c9ae1df94d317c4c5e087b21bc3
Taliesin Sisson <taliesins@yahoo.com>
2022-12-08 23:36:19 +0000

    Sort imports

--- internal/provider/schema_validators.go
+++ internal/provider/schema_validators.go
@@ -2,12 +2,13 @@ package provider
 
 import (
 	"fmt"
-	"github.com/hashicorp/go-cty/cty"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"math"
 	"reflect"
 	"strings"
+
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func stringKeyInMap(valid interface{}, ignoreCase bool) schema.SchemaValidateDiagFunc {
